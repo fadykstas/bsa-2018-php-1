1c749de373427bffca87e170576d5661c9439f61
fadykstas <stas.fadeev@gmail.com>
2018-05-11 16:02:53 +0300

    Ethereum class created

---
+++ src/Task1/Ethereum.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parallels
+ * Date: 5/11/18
+ * Time: 4:01 PM
+ */
+
+namespace Cryptocurrency\Task1;
+
+
+class Ethereum extends CoinAbstract
+{
+    protected $name = 'Ethereum';
+
+    protected $logoUrl = 'https://s2.coinmarketcap.com/static/img/coins/32x32/1027.png';
+}

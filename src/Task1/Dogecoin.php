aa4e798bbe92fefd6a86b1accc31450e2bdb81e8
fadykstas <stas.fadeev@gmail.com>
2018-05-11 16:04:26 +0300

    Dogecoin class created

---
+++ src/Task1/Dogecoin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parallels
+ * Date: 5/11/18
+ * Time: 4:03 PM
+ */
+
+namespace Cryptocurrency\Task1;
+
+
+class Dogecoin extends CoinAbstract
+{
+    protected $name = 'Dogecoin';
+
+    protected $logoUrl = 'https://s2.coinmarketcap.com/static/img/coins/32x32/74.png';
+}
